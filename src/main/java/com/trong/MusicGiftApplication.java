dd59788a82c167b2db1d2eaf9a1b27484f2aecdb
vonhutrong <vonhutrong@gmail.com>
2017-11-28 19:54:44 +0700

    Export war file Config to export war file instead of jar file.

--- src/main/java/com/trong/MusicGiftApplication.java
+++ src/main/java/com/trong/MusicGiftApplication.java
@@ -2,9 +2,16 @@ package com.trong;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MusicGiftApplication {
+public class MusicGiftApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MusicGiftApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MusicGiftApplication.class, args);
